d2d193aa9d54e3b66e2ad7eb68e0569ab2075452
Kemirdin <qemirdin@gmail.com>
2018-02-03 20:05:18 +0100

    create file

---
+++ Higher-order-functions/data.js
@@ -0,0 +1,39 @@
+export const animals = [{
+        name: 'Fluffykins',
+        species: 'rabbit'
+    },
+    {
+        name: 'Caro',
+        species: 'dog'
+    },
+    {
+        name: 'Hamilton',
+        species: 'dog'
+    },
+    {
+        name: 'Harold',
+        species: 'fish'
+    },
+    {
+        name: 'Ursula',
+        species: 'cat'
+    },
+    {
+        name: 'Jimmy',
+        species: 'fish'
+    }
+]
+
+export const orders = [{
+        amount: 250
+    },
+    {
+        amount: 400
+    },
+    {
+        amount: 100
+    },
+    {
+        amount: 325
+    }
+]
